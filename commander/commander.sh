190cb34910712d6d9c22711ce057c5db11e23d95
vmitris <victor.mitris@gmail.com>
2015-04-30 21:25:13 +0300

    added commander script

---
+++ commander/commander.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+while true; do
+	read -p "Enter command : " comm
+	if [[ "$comm" == "exit" ]];
+  		then 
+			break
+	fi
+	read -p "Enter parameters : " params
+	read -p "Destination for command : " dest
+    echo "Executing "$comm" "$params" "$dest""
+	final_comm="$comm $params"
+	ssh "$dest" "$final_comm" "> comm_output 2>&1 &"
+	echo "Command "$final_comm" was executed! at "$dest""	
+done
