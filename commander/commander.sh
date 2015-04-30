8e857f7cfe5ced581366d90ca70c0e4bb73f2cf0
vmitris <victor.mitris@gmail.com>
2015-04-30 22:02:54 +0300

    changed bad indentation

--- commander/commander.sh
+++ commander/commander.sh
@@ -7,7 +7,7 @@ while true; do
 	fi
 	read -p "Enter parameters : " params
 	read -p "Destination for command : " dest
-    echo "Executing "$comm" "$params" "$dest""
+	echo "Executing "$comm" "$params" "$dest""
 	final_comm="$comm $params"
 	ssh "$dest" "$final_comm" "> comm_output 2>&1 &"
 	echo "Command "$final_comm" was executed! at "$dest""	
