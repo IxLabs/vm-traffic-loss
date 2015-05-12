5ceb782214e099642319d9cb8c6fdb5e0ce14f4a
vmitris <victor.mitris@gmail.com>
2015-05-12 18:20:27 +0300

    changed initialization and xml saving for info structure

--- collector/info.cpp
+++ collector/info.cpp
@@ -2,8 +2,6 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 
-#define TestNumber 0
-
 /*functii pentru salvare structuri info*/
 void info_settings::load(const std::string &filename)
 {
@@ -12,6 +10,16 @@ void info_settings::load(const std::string &filename)
 
 void initialize(info_settings& statistics){
 	statistics.pdropped = 0;
+	statistics.reason = "";	
+	statistics.protocol.protocol = "fooProto";
+	statistics.protocol.sender = "0.0.0.0";
+	statistics.protocol.receiver = "0.0.0.0";
+	statistics.protocol.windowSize = TestNumber;
+	statistics.protocol.error.checkSum = TestNumber;
+	statistics.resources.memory_used = TestNumber;
+	statistics.resources.cpu_used = TestNumber;
+	statistics.resources.sockets_used = TestNumber;
+	statistics.resources.interrupts = TestNumber;
 }
 
 void info_settings::save(const std::string &filename)
@@ -24,15 +32,15 @@ void info_settings::save(const std::string &filename)
 	  	ptree dataTree;
 		dataTree.put("pdropped", pdropped);
 		dataTree.put("reason", reason);
-		dataTree.put("proto_details.protocol", "foo");
-		dataTree.put("proto_details.sender", "senderFoo");
-		dataTree.put("proto_details.receiver", "receiverBar");
-		dataTree.put("proto_details.windowSize", TestNumber);
-		dataTree.put("proto_details.errors.checksum", TestNumber);
-		dataTree.put("resources.Memory_used", TestNumber);
-		dataTree.put("resources.CPU_used", TestNumber);
-		dataTree.put("resources.Sockets_used", TestNumber);
-		dataTree.put("resources.Interrupts", TestNumber);
+		dataTree.put("proto_details.protocol", protocol.protocol);
+		dataTree.put("proto_details.sender", protocol.sender);
+		dataTree.put("proto_details.receiver", protocol.receiver);
+		dataTree.put("proto_details.windowSize", protocol.windowSize);
+		dataTree.put("proto_details.errors.checksum", protocol.error.checkSum);
+		dataTree.put("resources.Memory_used", resources.memory_used);
+		dataTree.put("resources.CPU_used", resources.cpu_used);
+		dataTree.put("resources.Sockets_used", resources.sockets_used);
+		dataTree.put("resources.Interrupts", resources.interrupts);
 		tree.add_child("statistics.info", dataTree);
 	  	write_xml(filename, tree);
 		fclose(file);
@@ -40,15 +48,15 @@ void info_settings::save(const std::string &filename)
 	else{
 		tree.put("statistics.info.pdropped", pdropped);
 		tree.put("statistics.info.reason", reason);		
-		tree.put("statistics.info.proto_details.protocol", "foo");
-		tree.put("statistics.info.proto_details.sender", "senderFoo");
-		tree.put("statistics.info.proto_details.receiver", "receiverBar");
-		tree.put("statistics.info.proto_details.windowSize", TestNumber);
-		tree.put("statistics.info.proto_details.errors.checksum", TestNumber);
-		tree.put("statistics.info.resources.Memory_used", TestNumber);
-		tree.put("statistics.info.resources.CPU_used", TestNumber);
-		tree.put("statistics.info.resources.Sockets_used", TestNumber);
-		tree.put("statistics.info.resources.Interrupts", TestNumber);
+		tree.put("statistics.info.proto_details.protocol", protocol.protocol);
+		tree.put("statistics.info.proto_details.sender", protocol.sender);
+		tree.put("statistics.info.proto_details.receiver", protocol.receiver);
+		tree.put("statistics.info.proto_details.windowSize", protocol.windowSize);
+		tree.put("statistics.info.proto_details.errors.checksum", protocol.error.checkSum);
+		tree.put("statistics.info.resources.Memory_used", resources.memory_used);
+		tree.put("statistics.info.resources.CPU_used", resources.cpu_used);
+		tree.put("statistics.info.resources.Sockets_used", resources.sockets_used);
+		tree.put("statistics.info.resources.Interrupts", resources.interrupts);
 	}
 	write_xml(filename, tree);
 }
--- collector/info.h
+++ collector/info.h
@@ -5,7 +5,24 @@
 #include <string>
 #include "iostream"
 
+#define TestNumber 0
+
 using namespace std;
+
+struct proto_errors
+{
+	public:
+		int checkSum;
+};
+
+struct resourcesUsed
+{
+	public:
+		int memory_used;
+		int cpu_used;
+		int sockets_used;
+		int interrupts;
+};
 
 struct prot_details
 {
@@ -13,6 +30,8 @@ struct prot_details
 		string protocol;
 		string sender;
 		string receiver;
+		int windowSize;
+		struct proto_errors error;	
 };
 
 struct info_settings
@@ -20,6 +39,7 @@ struct info_settings
 	int pdropped;
 	string reason;
 	struct prot_details protocol;
+	struct resourcesUsed resources;
 	void load(const std::string &filename);
 	void save(const std::string &filename);
 };
